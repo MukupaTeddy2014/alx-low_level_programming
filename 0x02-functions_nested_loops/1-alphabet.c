e1b434680e90fbcaf9f9387463f14024e80d4cbb
MukupaTeddy2014 <mukupateddy70@gmail.com>
2022-12-14 18:03:41 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i;
+
+	printf ("Lower case characters:\n");
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		printf ("%c ", i);
+	return (0);
+
+}
+
+
